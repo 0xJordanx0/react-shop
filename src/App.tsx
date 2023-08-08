2b55a52c01eef0fb559a60c11b81d8e648bb90f0
0xJordan <ahmedjamlan12@gmail.com>
2023-08-08 17:30:59 +0300

    Added Axios package

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import { useEffect, useState }from "react";
 
 export default function App() {
+  const [storeItems, setStoreItems] = useState([])
+
+  useEffect(()=> {
+    fetch("https://fakestoreapi.com/products").then((data) => console.log(data));
+  },[])
   return (
     <div>App</div>
   )
