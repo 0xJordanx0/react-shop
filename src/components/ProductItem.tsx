e83c586f101793090f945b23392d26ce9695306c
0xJordan <ahmedjamlan12@gmail.com>
2023-08-11 11:32:39 +0300

    Added Cart functionalities to the product page

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,9 +1,9 @@
 import { productItem } from "../types/types";
-
+import { NavLink } from "react-router-dom";
 export default function ProductItem(props: productItem) {
   return (
-    <div>
-    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+    <NavLink to={`/product/${props.id}`}>
+    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg xl:aspect-h-8 xl:aspect-w-7 hover:opacity-50">
       <img
         src={props.image}
         alt="product"
@@ -12,6 +12,6 @@ export default function ProductItem(props: productItem) {
     </div>
     <h3 className="mt-4 text-sm text-gray-700">{props.title}</h3>
     <p className="mt-1 text-lg font-medium text-gray-900">${props.price}</p>
-    </div>
+    </NavLink>
   );
 }
