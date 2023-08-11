3b8be8c32cd93299f59ace56110f7bd1ac42b097
0xJordan <ahmedjamlan12@gmail.com>
2023-08-11 11:33:29 +0300

    Modified AppLayout to be responsive

--- src/Layouts/AppLayout.tsx
+++ src/Layouts/AppLayout.tsx
@@ -1,18 +1,23 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Outlet, useNavigation } from "react-router-dom";
+import { CartProvider } from "../contexts/useContextCart";
 import Spinner from "../components/Spinner";
 export default function AppLayout() {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    console.log(navigation.state);
-    if (navigation.state === "loading") return "loading";
+  console.log(navigation.state);
+  if (navigation.state === "loading") return "loading";
 
   return (
-    <div>
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </div>
+    <>
+      <CartProvider>
+        <Navbar />
+        <div className="max-w-7xl mx-auto">
+          <Outlet />
+          <Footer />
+        </div>
+      </CartProvider>
+    </>
   );
 }
