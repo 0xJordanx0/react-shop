55a68036135aaf076300371887d8be5d38404c8d
0xJordan <ahmedjamlan12@gmail.com>
2023-08-11 16:05:55 +0300

    Removed paragraph element

--- src/contexts/useContextCart.tsx
+++ src/contexts/useContextCart.tsx
@@ -12,7 +12,6 @@ function CartProvider({children}) {
   return (
     <CartContext.Provider value={{cart, addItemToCart}}>
         {children}
-        <p>test</p>
     </CartContext.Provider>
   )
 }
